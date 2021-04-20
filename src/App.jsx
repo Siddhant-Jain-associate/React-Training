1d33cddc4623face2cb3765182069400dd070439
Siddhant-Jain-associate <siddhant.jain@successive.tech>
2021-04-20 12:34:42 +0530

    Day-5 react Training Complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,12 @@
 import React from 'react';
-import {useEffect,useState} from 'react';
 import Pages from './pages/index';
-import PropTypes from 'prop-types';
-import Slider from './components/Slider/Slider';
-import {getNextRoundRobin} from './libs/utils/math';
 
-function App(props) {
-
-  const path = ['cloud.jpg','default.png','dns-server.png','full-stack-web-development.jpg','js.jpg','load-balancer.png'];
-
-  const [images,setImages] = useState([]);
-  const [defaultImage,setDefaultImages] = useState('/images/default.png');
-  const [duration,setDuration] = useState(2000);
-  const [height,setHeight]= useState(200);
-  const [random,setRandom] = useState(false);
-  useEffect(() => {
-    let interval = setInterval(getNextRoundRobin,2000);
-    console.log(interval);
-    return () => {
-      clearInterval(interval);
-    }
-  },[]);
-  
+function App() {
   return (
     <div>
-            <Slider defaultBanner={defaultImage} altText="Default Banner" random={random.toString()} height={props.height}/>
-             <Pages />
+      <Pages />
     </div>
   );
-}
-
-App.propTypes = {
-  defaultBanner:PropTypes.string,
-  height:PropTypes.number,
-  duration:PropTypes.number,
-  banners:PropTypes.array,
-  random:PropTypes.bool,
 }
 
 export default App;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import TextFieldDemo from './TextFieldDemo/TextFieldDemo';
-
+import Slider from './SliderDemo/SliderDemo';
 const Pages = () =>{
     return (
         <div>
+            <Slider />
             <TextFieldDemo />
+        
         </div>
     )
 }
